e6fa66befa36163f6bd44a28995c9d01e4158b83
Neil Srivastava <neil.srivastava@benchling.com>
2024-12-01 19:44:55 -0800

    pre-commit

--- server/authn/api/login.py
+++ server/authn/api/login.py
@@ -1,13 +1,18 @@
-
 from fastapi import APIRouter, Depends
 
-from server.dependencies import RequestDependenciesBase, get_unauthenticated_request_dependencies
+from server.dependencies import (
+    RequestDependenciesBase,
+    get_unauthenticated_request_dependencies,
+)
 
 router = APIRouter()
+
 
 @router.post("/login")
 async def login(
     req_body: dict,
-    req_dep: RequestDependenciesBase = Depends(get_unauthenticated_request_dependencies),
+    req_dep: RequestDependenciesBase = Depends(
+        get_unauthenticated_request_dependencies
+    ),
 ) -> None:
     pass
